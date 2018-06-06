f2064fcfdb435df08e71c150c55af17e0827e36c
Noah Harrigan, a good Christian boy <36709150+JesusLover123@users.noreply.github.com>
2018-06-05 23:32:41 -0400

    Add files via upload

--- Platform.java
+++ Platform.java
@@ -1,113 +1,3 @@
-//import java.awt.geom.Rectangle2D;
-//import java.awt.image.BufferedImage;
-//import java.io.File;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//
-//import javax.imageio.ImageIO;
-//import javax.swing.Icon;
-//import javax.swing.ImageIcon;
-//import javax.swing.JLabel;
-//
-//public class Platform extends JLabel
-//{
-//	private BufferedImage image;
-//	private Rectangle2D size;
-//	
-//	private static ArrayList<Platform> platforms = new ArrayList<Platform>();
-//	
-//	public Platform(int x, int y)
-//	{
-//		image = null;
-//		try
-//		{
-//			image = ImageIO.read(new File("platform image.jpg"));
-//		}
-//		catch(IOException e1)
-//		{
-//			e1.printStackTrace();
-//		}
-//		ImageIcon icon = new ImageIcon(image);
-////		base1l.setPreferredSize(new Dimension(base1.getHeight(), base1.getWidth()));
-//		setBounds(x, y, image.getWidth(), image.getHeight());
-//		size = new Rectangle2D.Double(getX(), getY(), image.getWidth(), image.getHeight());
-//		platforms.add(this);
-//		setVisible(true);
-//		setIcon(icon);
-//
-//	}
-//	
-//	public Platform(int x, int y, int x1)
-//	{
-//		super();
-//		image = null;
-//		try
-//		{
-//			image = ImageIO.read(new File("platform image.jpg"));
-//		}
-//		catch(IOException e1)
-//		{
-//			e1.printStackTrace();
-//		}
-//		ImageIcon image1 = new ImageIcon(image);
-////		base1l.setPreferredSize(new Dimension(base1.getHeight(), base1.getWidth()));
-//		setBounds(x, y, x1, 92);
-//		setVisible(true);
-//		setIcon(image1);
-//		platforms.add(this);
-//	}
-//	
-//	public Platform(int x, int y, String yeahboi)
-//	{
-//		super();
-//		image = null;
-//		try
-//		{
-//			image = ImageIO.read(new File("cityplat.png"));
-//		}
-//		catch(IOException e1)
-//		{
-//			e1.printStackTrace();
-//		}
-//		ImageIcon image1 = new ImageIcon(image);
-////		base1l.setPreferredSize(new Dimension(base1.getHeight(), base1.getWidth()));
-//		setBounds(x, y, image.getWidth(), image.getHeight());
-//		setVisible(true);
-//		setIcon(image1);
-//		platforms.add(this);
-//	}
-//	
-//	public Platform(int x, int y, int x1, int yeahboi)
-//	{
-//		super();
-//		image = null;
-//		try
-//		{
-//			image = ImageIO.read(new File("cityplat.png"));
-//		}
-//		catch(IOException e1)
-//		{
-//			e1.printStackTrace();
-//		}
-//		ImageIcon image1 = new ImageIcon(image);
-////		base1l.setPreferredSize(new Dimension(base1.getHeight(), base1.getWidth()));
-//		setBounds(x, y, x1, image.getHeight());
-//		setVisible(true);
-//		setIcon(image1);
-//		platforms.add(this);
-//	}
-//	
-//	public Rectangle2D getRect()
-//	{
-//		return size;
-//	}
-//	
-//	public static ArrayList<Platform> getPlatforms()
-//	{
-//		return platforms;
-//	}
-//
-//}
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -128,6 +18,7 @@ public class Platform extends JLabel
 	
 	public Platform(int x, int y)
 	{
+		image = null;
 		try
 		{
 			image = ImageIO.read(new File("platform image.jpg"));
@@ -146,8 +37,10 @@ public class Platform extends JLabel
 
 	}
 	
-	public Platform(int x, int y, int x1, int y1)
+	public Platform(int x, int y, int x1)
 	{
+		super();
+		image = null;
 		try
 		{
 			image = ImageIO.read(new File("platform image.jpg"));
@@ -156,13 +49,52 @@ public class Platform extends JLabel
 		{
 			e1.printStackTrace();
 		}
-
+		ImageIcon image1 = new ImageIcon(image);
 //		base1l.setPreferredSize(new Dimension(base1.getHeight(), base1.getWidth()));
-		setBounds(x, y, x1, y1);
-		size = new Rectangle2D.Double(getX(), getY(), image.getWidth(), image.getHeight());
-		platforms.add(this);
+		setBounds(x, y, x1, 92);
 		setVisible(true);
-		setIcon((Icon)image);
+		setIcon(image1);
+		platforms.add(this);
+	}
+	
+	public Platform(int x, int y, String yeahboi)
+	{
+		super();
+		image = null;
+		try
+		{
+			image = ImageIO.read(new File("cityplat.png"));
+		}
+		catch(IOException e1)
+		{
+			e1.printStackTrace();
+		}
+		ImageIcon image1 = new ImageIcon(image);
+//		base1l.setPreferredSize(new Dimension(base1.getHeight(), base1.getWidth()));
+		setBounds(x, y, image.getWidth(), image.getHeight());
+		setVisible(true);
+		setIcon(image1);
+		platforms.add(this);
+	}
+	
+	public Platform(int x, int y, int x1, int yeahboi)
+	{
+		super();
+		image = null;
+		try
+		{
+			image = ImageIO.read(new File("cityplat.png"));
+		}
+		catch(IOException e1)
+		{
+			e1.printStackTrace();
+		}
+		ImageIcon image1 = new ImageIcon(image);
+//		base1l.setPreferredSize(new Dimension(base1.getHeight(), base1.getWidth()));
+		setBounds(x, y, x1, image.getHeight());
+		setVisible(true);
+		setIcon(image1);
+		platforms.add(this);
 	}
 	
 	public Rectangle2D getRect()
@@ -174,4 +106,5 @@ public class Platform extends JLabel
 	{
 		return platforms;
 	}
+
 }
